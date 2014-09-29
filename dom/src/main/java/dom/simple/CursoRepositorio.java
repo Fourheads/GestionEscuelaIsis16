4b7a8eaa269caa18b553328f50bb700694c5c9b8
Leandro Torroija <leanddro@hotmail.com>
2014-09-29 11:46:35 -0300

    Asignar profesor a Materia de un Curso
    Creado solo la accion, con los dropDownLists. Aun faltan todos los metodos default, choices y validate

--- dom/src/main/java/dom/simple/CursoRepositorio.java
+++ dom/src/main/java/dom/simple/CursoRepositorio.java
@@ -19,39 +19,36 @@ import dom.planEstudio.PlanRepositorio;
 public class CursoRepositorio {
 
 	@MemberOrder(sequence = "1")
-	public Curso crearCurso(Plan plan, 
-							Anio anio, 
-							@Named("División") String division,
-							@Named("Turno") Turno turno) {
+	public Curso crearCurso(Plan plan, Anio anio,
+			@Named("División") String division, @Named("Turno") Turno turno) {
 
 		Curso curso = container.newTransientInstance(Curso.class);
 		curso.setAnio(anio);
 		curso.setDivision(division);
 		curso.setTurno(turno);
-				
+
 		for (Materia materia : anio.getMateriaList()) {
 
 			materiaDelCursoRepositorio.crearMateriaDelCurso(curso, materia);
-			
+
 		}
 
 		container.persistIfNotAlready(curso);
 		return curso;
 	}
-	
+
 	public List<Plan> choices0CrearCurso() {
 
 		return planRepositorio.queryListarPlanesAlfabeticamente();
 	}
 
 	public Plan default0CrearCurso() {
-		if (choices0CrearCurso().isEmpty()){
+		if (choices0CrearCurso().isEmpty()) {
 			return null;
 		}
-		
+
 		return choices0CrearCurso().get(0);
 	}
-	
 
 	// {{ listarCursosDeUnPlan (action)
 	@MemberOrder(sequence = "2")
@@ -67,64 +64,73 @@ public class CursoRepositorio {
 	}
 
 	public Plan default0ListarCursosDeUnPlan() {
-		if (choices0ListarCursosDeUnPlan().isEmpty()){
+		if (choices0ListarCursosDeUnPlan().isEmpty()) {
 			return null;
 		}
-		
+
 		return choices0ListarCursosDeUnPlan().get(0);
 	}
-	
+
 	// {{ listarCursosDeUnAnio (action)
-		@MemberOrder(sequence = "2.5")
-		public List<Curso> listarCursosDeUnAnio(@Named("Plan") final Plan plan, 
-												@Named("Año") final Anio anio) {
+	@MemberOrder(sequence = "2.5")
+	public List<Curso> listarCursosDeUnAnio(@Named("Plan") final Plan plan,
+			@Named("Año") final Anio anio) {
 
-			return container.allMatches(new QueryDefault<Curso>(Curso.class,
-					"listarCursosDeUnAnio", 
-					"plan", plan.getDescripcion(),
-					"anio", anio.getAnioNumero()));
-		}
-	
-		public List<Plan> choices0ListarCursosDeUnAnio() {
+		return container.allMatches(new QueryDefault<Curso>(Curso.class,
+				"listarCursosDeUnAnio", "plan", plan.getDescripcion(), "anio",
+				anio.getAnioNumero()));
+	}
 
-			return planRepositorio.queryListarPlanesAlfabeticamente();
+	public List<Plan> choices0ListarCursosDeUnAnio() {
+
+		return planRepositorio.queryListarPlanesAlfabeticamente();
+	}
+
+	public Plan default0ListarCursosDeUnAnio() {
+		if (choices0ListarCursosDeUnAnio().isEmpty()) {
+			return null;
 		}
 
-		public Plan default0ListarCursosDeUnAnio() {
-			if (choices0ListarCursosDeUnAnio().isEmpty()){
-				return null;
-			}
-			
-			return choices0ListarCursosDeUnAnio().get(0);
-		}	
-		
-		public List<Anio> choices1ListarCursosDeUnAnio(final Plan plan) {
-			return plan.getAnioList();
-		}
-		
-		
+		return choices0ListarCursosDeUnAnio().get(0);
+	}
+
+	public List<Anio> choices1ListarCursosDeUnAnio(final Plan plan) {
+		return plan.getAnioList();
+	}
+
 	// {{ SeleccionarUnCurso (action)
 	@MemberOrder(sequence = "1")
-	public Curso seleccionarUnCurso(Plan plan, 
-							Curso curso 
-							) {
+	public Curso seleccionarUnCurso(Plan plan, Curso curso) {
 		return curso;
 	}
-	
+
 	public List<Plan> choices0SeleccionarUnCurso() {
 
 		return planRepositorio.queryListarPlanesAlfabeticamente();
 	}
 
 	public Plan default0SeleccionarUnCurso() {
-		if (choices0SeleccionarUnCurso().isEmpty()){
+		if (choices0SeleccionarUnCurso().isEmpty()) {
 			return null;
 		}
-		
+
 		return choices0SeleccionarUnCurso().get(0);
 	}
 	
-	
+	@Named("Asignar Profesor a Materia")
+	@MemberOrder(sequence = "4")
+	public Curso asignarProfesorAMateriaDelCurso(
+			@Named("Plan") final Plan plan,
+			@Named("Curso") final Curso curso,
+			@Named("Materia") final MateriaDelCurso materia,
+			@Named("Profesor") final Personal profesor) {
+
+		materiaDelCursoRepositorio.asignarProfesorAMateriaDelCurso(materia,
+				profesor);
+
+		return materia.getCurso();
+	}
+
 	// }}
 
 	// region > injected services
--- dom/src/main/java/dom/simple/MateriaDelCursoRepositorio.java
+++ dom/src/main/java/dom/simple/MateriaDelCursoRepositorio.java
@@ -1,17 +1,24 @@
 package dom.simple;
 
 import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.Hidden;
 import org.apache.isis.applib.annotation.Programmatic;
 
 import dom.planEstudio.Materia;
 @DomainService
 public class MateriaDelCursoRepositorio {
 	
-	@Programmatic
+	@Hidden
 	public void crearMateriaDelCurso (Curso curso, Materia materia){
 		MateriaDelCurso materiaDelCurso = new MateriaDelCurso();
 		materiaDelCurso.setMateria(materia);
 		curso.getMateriaDelCursoList().add(materiaDelCurso);
 	}
+	
+	@Hidden
+	public void asignarProfesorAMateriaDelCurso(MateriaDelCurso materia, Personal profesor){
+		
+		materia.setProfesor(profesor);
+	} 
 
 }
