67b47db8b7b51b33f4de98e44493465d268989e8
Mingorth <Mingorth@gmail.com>
2014-10-12 17:49:47 -0300

    Clase LibroDiarioRepositorio:
    -Se agrego la licencia correspondiente.
    -Se agregaron los metodos crear, modificar y listar.
    - Ver con atencian metodo modificar, puede trar incnvenientes a la hora
    de crear nuevas materias del plan.
    -Se agregaron las anotaciones @Hiden en algunos metodos que son de acceso
    exclusivo del programa.

--- dom/src/main/java/dom/libroDiario/LibroDiarioRepositorio.java
+++ dom/src/main/java/dom/libroDiario/LibroDiarioRepositorio.java
@@ -1,3 +1,27 @@
+/*
+ * This is a software made for highschool management 
+ * 
+ * Copyright (C) 2014, Fourheads
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * 
+ * 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+
+
 package dom.libroDiario;
 
 import org.apache.isis.applib.DomainObjectContainer;
@@ -6,17 +30,31 @@ import org.apache.isis.applib.annotation.Hidden;
 import org.apache.isis.applib.annotation.MemberOrder;
 import org.apache.isis.applib.query.QueryDefault;
 
+import java.util.List;
+
 import dom.simple.Curso;
+
 
 @DomainService(menuOrder = "100")
 public class LibroDiarioRepositorio {
 	
 	
 	@Hidden
-	public void crearLibroDiario(Curso curso) {
+	//@MemberOrder(sequence = "1")
+	public void crearLibroDiario(final Curso curso) {
 		
 		LibroDiario libroDiario = container.newTransientInstance(LibroDiario.class);
 		libroDiario.setCurso(curso);
+		libroDiario.setMateriaDelLibroDiarioList(materiaDelLibroDiarioRepositorio.crearListaMateriaDelLibroDiario(curso));
+		libroDiario.settiutulo(curso.title());
+		container.persistIfNotAlready(libroDiario);
+		
+	}
+	
+	@Hidden
+	public void ModificarLibroDiario(final Curso curso, final LibroDiario libroDiario) {
+		
+		//libroDiario.setCurso(curso);
 		libroDiario.setMateriaDelLibroDiarioList(materiaDelLibroDiarioRepositorio.crearListaMateriaDelLibroDiario(curso));
 		container.persistIfNotAlready(libroDiario);
 		
@@ -34,7 +72,12 @@ public class LibroDiarioRepositorio {
 	}
 	// }}
 
-
+	@Hidden
+	@MemberOrder(sequence = "2")
+	public List<LibroDiario> listaLibroDiarioDelCurso() {
+		
+		return container.allMatches(new QueryDefault<LibroDiario>(LibroDiario.class, "MateriasDeUnLibroDiario"));
+	}
 	
 	@javax.inject.Inject
 	DomainObjectContainer container;
