2f2fe6f78a2bc175c6902ad44010d1b9b48d2b46
Mingorth <Mingorth@gmail.com>
2014-10-12 17:42:22 -0300

    Clase EntradaLikbroDiariRepositorio:
    -Se agrego la licencia correspondiente.
    -Se agregaron los metodos crear entrada y listar entrada.
    -Se agrego @Hidden para los metodos ocultos al menu.

--- dom/src/main/java/dom/libroDiario/EntradadeLibroDiarioRepositorio.java
+++ dom/src/main/java/dom/libroDiario/EntradadeLibroDiarioRepositorio.java
@@ -1,23 +1,60 @@
+/*
+ * This is a software made for highschool management 
+ * 
+ * Copyright (C) 2014, Fourheads
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * 
+ * 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
 package dom.libroDiario;
+
+import java.util.List;
 
 import org.apache.isis.applib.DomainObjectContainer;
 import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.Hidden;
+import org.apache.isis.applib.annotation.MaxLength;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.MultiLine;
 import org.apache.isis.applib.annotation.Named;
+import org.apache.isis.applib.query.QueryDefault;
 import org.joda.time.LocalDate;
 
-@Named("Entrada libro diario")
+import dom.simple.Curso;
+import dom.simple.MateriaDelCurso;
+import dom.simple.MateriaDelCursoRepositorio;
+
+//@Named("Entrada libro diario")
+@Hidden
 @DomainService(repositoryFor = EntradaLibroDiario.class)
 public class EntradadeLibroDiarioRepositorio {
 
 	public EntradaLibroDiario crearEntradadeLibroDiario(LocalDate fecha,
-			int horas, int unidad, String actividad, String observaciones) {
+			int horas, int unidad, final @MaxLength(2048)
+	  @MultiLine @Named("Actividad") String actividad,
+	  final @MaxLength(2048)
+	  @MultiLine@Named("Observaciones") String Observaciones) {
 		
 		EntradaLibroDiario entradalibrodiario = container.newTransientInstance(EntradaLibroDiario.class);
 		
 
 		entradalibrodiario.setFecha(fecha);
 		
-		HoraCatedra horacate= horacatedra.crearhoracatedra(horas, unidad, actividad, observaciones);
+		HoraCatedra horacate= horacatedra.crearhoracatedra(horas, unidad, actividad, Observaciones);
 		
 		entradalibrodiario.asignarHoracatedra(horacate);
 		
@@ -34,9 +71,35 @@ public class EntradadeLibroDiarioRepositorio {
 		return fecha;
 	}
 	
+	@Hidden
+	@MemberOrder(sequence = "1")
+	public List<EntradaLibroDiario> TodasLasEntradas()
+	{
+		return container.allMatches(new QueryDefault<EntradaLibroDiario>(EntradaLibroDiario.class,
+				"TodasEntradaLibroDiarioList"));
+	}
+	
+	@MemberOrder(sequence = "2")
+	public List<EntradaLibroDiario> entradasporfecha(@Named("Curso") final Curso curso, @Named("Materia") final MateriaDelCurso materiadelcurso, @Named("Fecha") final LocalDate fecha)
+	{
+		return container.allMatches(new QueryDefault<EntradaLibroDiario>(EntradaLibroDiario.class,
+				"EntradaLibroDiarioListFecha", "Fecha", fecha, "materiacurso", materiadelcurso));
+	}
+	
+	/*
+	public List<MateriaDelCurso> choices1Entradasporfecha(@Named("Curso") final Curso curso)
+	{
+		if(curso != null)
+			return materiaDelCursoRepositorio.listarMateriaDelCursoParaUnCurso(curso);
+		else
+			return null;
+	}*/
+
 	@javax.inject.Inject
 	DomainObjectContainer container;
 	@javax.inject.Inject
 	HoraCatedraRepositorio horacatedra; 
+	@javax.inject.Inject
+	MateriaDelCursoRepositorio materiaDelCursoRepositorio;
 	
 }
