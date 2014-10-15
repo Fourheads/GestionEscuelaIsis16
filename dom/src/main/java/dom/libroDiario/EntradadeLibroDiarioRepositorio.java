91a6c486c40d874124b943c4e431774bf624ee8f
Mingorth <Mingorth@gmail.com>
2014-10-15 09:02:28 -0300

    Clase EntradadeLibroDiario: Agregado metanotaciones Named y render agregados

--- dom/src/main/java/dom/libroDiario/EntradadeLibroDiarioRepositorio.java
+++ dom/src/main/java/dom/libroDiario/EntradadeLibroDiarioRepositorio.java
@@ -31,6 +31,7 @@ import org.apache.isis.applib.annotation.MaxLength;
 import org.apache.isis.applib.annotation.MemberOrder;
 import org.apache.isis.applib.annotation.MultiLine;
 import org.apache.isis.applib.annotation.Named;
+import org.apache.isis.applib.annotation.Render;
 import org.apache.isis.applib.query.QueryDefault;
 import org.joda.time.LocalDate;
 
@@ -43,6 +44,8 @@ import dom.simple.MateriaDelCursoRepositorio;
 @DomainService(repositoryFor = EntradaLibroDiario.class)
 public class EntradadeLibroDiarioRepositorio {
 
+	@Hidden
+	@Render
 	public EntradaLibroDiario crearEntradadeLibroDiario(LocalDate fecha,
 			int horas, int unidad, final @MaxLength(2048)
 	  @MultiLine @Named("Actividad") String actividad,
@@ -72,13 +75,15 @@ public class EntradadeLibroDiarioRepositorio {
 	}
 	
 	@Hidden
-	@MemberOrder(sequence = "1")
+	//@MemberOrder(sequence = "1")
 	public List<EntradaLibroDiario> TodasLasEntradas()
 	{
 		return container.allMatches(new QueryDefault<EntradaLibroDiario>(EntradaLibroDiario.class,
 				"TodasEntradaLibroDiarioList"));
 	}
 	
+	@Render
+	@Named("Entradas por fecha")
 	@MemberOrder(sequence = "2")
 	public List<EntradaLibroDiario> entradasporfecha(@Named("Curso") final Curso curso, @Named("Materia") final MateriaDelCurso materiadelcurso, @Named("Fecha") final LocalDate fecha)
 	{
