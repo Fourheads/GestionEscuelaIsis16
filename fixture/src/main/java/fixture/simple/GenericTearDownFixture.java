d5f1795c4331ddd89f9fc53c37615def0e8b16d7
Mingorth <Mingorth@gmail.com>
2014-11-01 17:57:09 -0300

    Clase GenericTearDownFixture: Se cambio la forma de implementar el metodo execute.

--- fixture/src/main/java/fixture/simple/GenericTearDownFixture.java
+++ fixture/src/main/java/fixture/simple/GenericTearDownFixture.java
@@ -19,25 +19,24 @@
 
 package fixture.simple;
 
-
 import org.apache.isis.applib.fixturescripts.FixtureScript;
 import org.apache.isis.objectstore.jdo.applib.service.support.IsisJdoSupport;
 
 public class GenericTearDownFixture extends FixtureScript {
 
 	private String Clase;
-		
+	
 	GenericTearDownFixture(String clase)
 	{
-		Clase=clase;
+		Clase = clase;
 	}
-	
+		
     @Override
     protected void execute(ExecutionContext executionContext) {
-    		isisJdoSupport.executeUpdate("delete from \""+Clase+"\"");
+    		isisJdoSupport.executeUpdate("TRUNCATE \""+Clase+"\" CASCADE");
     }
 
-
+    
     @javax.inject.Inject
     private IsisJdoSupport isisJdoSupport;
 
