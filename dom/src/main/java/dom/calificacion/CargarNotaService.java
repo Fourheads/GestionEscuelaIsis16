58a49878488e7d85555114d7b4d13e98aa6e8523
Leandro Torroija <leanddro@hotmail.com>
2014-11-08 12:46:17 -0300

    Arreglar choices

--- dom/src/main/java/dom/calificacion/CargarNotaService.java
+++ dom/src/main/java/dom/calificacion/CargarNotaService.java
@@ -42,131 +42,151 @@ import dom.simple.MateriaDelCurso;
 @Named("Cargar Notas")
 @DomainService(menuOrder = "80")
 public class CargarNotaService {
-	
-	  @Hidden
-		public String getId() {
-	        return "cargarNotas";
-	    }
 
-	    public String title(){
-	    	return "Cargar Notas";
-	    }
-	    
-	    public String iconName() {
-	        return "SimpleObject";
-	    }
-	    @Named("Por Curso")
-	    @MemberOrder(name = "Cargar Notas" , sequence = "1")
-	    //@Hidden(where = Where.)
-	    public CargarNotaView calificarPorCurso(final @Named("Ciclo: ") Calificaciones inCalificacion,
-	    										final @Named("Periodo: ") Periodo inPeriodo,	    																						 
-	    										final @Named("Curso: ") Curso inCurso,
-	    										final @Named("Materia: ") MateriaDelCurso inMateria){
-	    	
-	    	Memento newMemento = mementoService.create();
-	    	//titulo, periodo, anio, division, materia, plan, alumnoIndex
-	    	newMemento.set("titulo", "Cargar Nota");
-	    	newMemento.set("periodo", inPeriodo.getNombre());
-	    	newMemento.set("anio", inCurso.getAnio().getAnioNumero());
-	    	newMemento.set("division", inCurso.getDivision());
-	    	newMemento.set("materia", inMateria.getMateria().getNombre());
-	    	newMemento.set("plan", inCurso.getAnio().getPlan().getDescripcion());
-	    	newMemento.set("indiceAlumno", 0);
-	    	
-	    	//titulo, periodo, anio, division, materia, plan, alumnoIndex
-	    		    	
-	    	final CargarNotaView newView = container.newViewModelInstance(CargarNotaView.class, newMemento.asString());
-	    	
-	    	return newView;
-	    	
-	    }
-	    
-	    public List<Calificaciones> choices0CalificarPorCurso(final @Named("Ciclo: ") Calificaciones inCalificacion) {
-	    	
-	    	List<Calificaciones> listCalificaciones = califRepositorio.ciclosConPeriodo();
-	    	if(listCalificaciones.isEmpty()){
-	    		container.warnUser("Debe crear al menos un ciclo lectivo con sus períodos de evaluación");
-	    		return listCalificaciones;
-	    	}
-	    	return listCalificaciones;
+	@Hidden
+	public String getId() {
+		return "cargarNotas";
+	}
+
+	public String title() {
+		return "Cargar Notas";
+	}
+
+	public String iconName() {
+		return "SimpleObject";
+	}
+
+	@Named("Por Curso")
+	@MemberOrder(name = "Cargar Notas", sequence = "1")
+	// @Hidden(where = Where.)
+	public CargarNotaView calificarPorCurso(
+			final @Named("Ciclo: ") Calificaciones inCalificacion,
+			final @Named("Periodo: ") Periodo inPeriodo,
+			final @Named("Curso: ") Curso inCurso,
+			final @Named("Materia: ") MateriaDelCurso inMateria) {
+
+		Memento newMemento = mementoService.create();
+		// titulo, periodo, anio, division, materia, plan, alumnoIndex
+		newMemento.set("titulo", "Cargar Nota");
+		newMemento.set("periodo", inPeriodo.getNombre());
+		newMemento.set("anio", inCurso.getAnio().getAnioNumero());
+		newMemento.set("division", inCurso.getDivision());
+		newMemento.set("materia", inMateria.getMateria().getNombre());
+		newMemento.set("plan", inCurso.getAnio().getPlan().getDescripcion());
+		newMemento.set("indiceAlumno", 0);
+
+		// titulo, periodo, anio, division, materia, plan, alumnoIndex
+
+		final CargarNotaView newView = container.newViewModelInstance(
+				CargarNotaView.class, newMemento.asString());
+
+		return newView;
+
+	}
+
+	public List<Calificaciones> choices0CalificarPorCurso(
+			final @Named("Ciclo: ") Calificaciones inCalificacion) {
+
+		List<Calificaciones> listCalificaciones = califRepositorio
+				.ciclosConPeriodo();
+		if (listCalificaciones.isEmpty()) {
+			container
+					.warnUser("Debe crear al menos un ciclo lectivo con sus períodos de evaluación");
+			return listCalificaciones;
 		}
-	    
-	    public Calificaciones default0CalificarPorCurso(final @Named("Ciclo: ") Calificaciones inCalificacion) {
-			if (choices0CalificarPorCurso(inCalificacion).isEmpty()) {
-				return null;
-			}
-			return choices0CalificarPorCurso(inCalificacion).get(0);
+		return listCalificaciones;
+	}
+
+	/*public Calificaciones default0CalificarPorCurso(
+			final @Named("Ciclo: ") Calificaciones inCalificacion) {
+		if (choices0CalificarPorCurso(inCalificacion).isEmpty()) {
+			return null;
 		}
-	    
-	    public List<Periodo> choices1CalificarPorCurso(final @Named("Ciclo: ") Calificaciones inCalificacion
-														/*final @Named("Periodo: ") Periodo inPeriodo*/){
-	    	
-	    	List<Periodo> periodos = perRepo.periodoPorCiclo(inCalificacion.getCicloCalificacion());
-	    	if(periodos.isEmpty()){
-	    		container.warnUser("Debe crear al menos un ciclo lectivo con sus períodos de evaluación");
-	    		return null;
-	    	}
-	    	return periodos;
-	    }
-	    
-	    /*public Periodo default1CalificarPorCurso(final @Named("Ciclo: ") Calificaciones inCalificacion,
-												final @Named("Periodo: ") Periodo inPeriodo) {
-			if (choices1CalificarPorCurso(inCalificacion, inPeriodo).isEmpty()) {
-				return null;
+		return choices0CalificarPorCurso(inCalificacion).get(0);
+	}*/
+
+	public List<Periodo> choices1CalificarPorCurso(
+			final @Named("Ciclo: ") Calificaciones inCalificacion
+	/* final @Named("Periodo: ") Periodo inPeriodo */) {
+
+		List<Periodo> periodos = perRepo.periodoPorCiclo(inCalificacion
+				.getCicloCalificacion());
+		if (periodos.isEmpty()) {
+			container
+					.warnUser("Debe crear al menos un ciclo lectivo con sus períodos de evaluación");
+			return null;
+		}
+		return periodos;
+	}
+
+	/*
+	 * public Periodo default1CalificarPorCurso(final @Named("Ciclo: ")
+	 * Calificaciones inCalificacion, final @Named("Periodo: ") Periodo
+	 * inPeriodo) { if (choices1CalificarPorCurso(inCalificacion,
+	 * inPeriodo).isEmpty()) { return null; } return
+	 * choices1CalificarPorCurso(inCalificacion, inPeriodo).get(0); }
+	 */
+
+	// VERIFICAR FUNCIONAMIENTO DE CHOICES
+	public List<Curso> choices2CalificarPorCurso(
+			final @Named("Ciclo: ") Calificaciones inCalificacion,
+			final @Named("Periodo: ") Periodo inPeriodo,
+			final @Named("Curso: ") Curso inCurso,
+			final @Named("Materia: ") MateriaDelCurso inMateria) {
+		List<Curso> cursos = new ArrayList<Curso>();
+		if (inPeriodo != null) {
+			cursos = aluCalRepositorio.listCursoPorPeriodo(inPeriodo);
+		}
+
+		if (cursos.isEmpty()) {
+			return null;
+		}
+
+		return cursos;
+	}
+
+	/*
+	 * public Curso default2CalificarPorCurso(final @Named("Periodo: ") Periodo
+	 * inPeriodo){ if(choices2CalificarPorCurso(inPeriodo).isEmpty()){ return
+	 * null; } return choices2CalificarPorCurso(inPeriodo).get(0); }
+	 */
+
+	public List<MateriaDelCurso> choices3CalificarPorCurso(
+			final @Named("Ciclo: ") Calificaciones inCalificacion,
+			final @Named("Periodo: ") Periodo inPeriodo,
+			final @Named("Curso: ") Curso inCurso,
+			final @Named("Materia: ") MateriaDelCurso inMateria) {
+
+		if (inCurso != null) {
+			if (!inCurso.getMateriaDelCursoList().isEmpty()) {
+				return inCurso.getMateriaDelCursoList();
 			}
-			return choices1CalificarPorCurso(inCalificacion, inPeriodo).get(0);
-		}*/
-	    
-	    
-	    //VERIFICAR FUNCIONAMIENTO DE CHOICES
-	    /*public List<Curso> choices2CalificarPorCurso(final @Named("Ciclo: ") Calificaciones inCalificacion,
-													final @Named("Periodo: ") Periodo inPeriodo	    																						 
-													final @Named("Curso: ") Curso inCurso){
-	    	List<Curso> cursos = aluCalRepositorio.listCursoPorPeriodo(inPeriodo);	    	
-	    	if(cursos.isEmpty()){
-	    		return null;
-	    	}
-	    	
-	    	return cursos;
-	    }*/
-	    
-	    /*public Curso default2CalificarPorCurso(final @Named("Periodo: ") Periodo inPeriodo){
-	    	if(choices2CalificarPorCurso(inPeriodo).isEmpty()){
-	    		return null;
-	    	}
-	    	return choices2CalificarPorCurso(inPeriodo).get(0);
-	    }*/
-	    
-	   /*public List<MateriaDelCurso> choices3CalificarPorCurso(final @Named("Curso: ") Curso inCurso){		   
-		   if(!inCurso.getMateriaDelCursoList().isEmpty()){
-			   return inCurso.getMateriaDelCursoList();   
-		   }
-		   return null;	    	
-	    }*/
-	    
-	    /*public MateriaDelCurso default3CalificarPorCurso(Curso curso){
-	    	if(choices3CalificarPorCurso(curso).isEmpty()){
-	    		return null;
-	    	}
-	    	return choices3CalificarPorCurso(curso).get(0);
-	    }*/
-	    
-	    
-	    @javax.inject.Inject
-		DomainObjectContainer container;
-	    	    
-	    @javax.inject.Inject
-	    CursoRepositorio cursoRepositorio;
-	    MateriaCalificacionRepositorio mcRepositorio;
-	    @javax.inject.Inject
-	    MementoService mementoService;
-	    
-	    @javax.inject.Inject
-		AlumnoCalificacionRepositorio aluCalRepositorio;
-	    
-	    @javax.inject.Inject
-	    CalificacionRepositorio califRepositorio;
-	    
-	    @javax.inject.Inject
-	    PeriodoRepositorio perRepo;
+		}
+		return null;
+
+	}
+
+	/*
+	 * public MateriaDelCurso default3CalificarPorCurso(Curso curso){
+	 * if(choices3CalificarPorCurso(curso).isEmpty()){ return null; } return
+	 * choices3CalificarPorCurso(curso).get(0); }
+	 */
+
+	@javax.inject.Inject
+	DomainObjectContainer container;
+
+	@javax.inject.Inject
+	CursoRepositorio cursoRepositorio;
+	MateriaCalificacionRepositorio mcRepositorio;
+	@javax.inject.Inject
+	MementoService mementoService;
+
+	@javax.inject.Inject
+	AlumnoCalificacionRepositorio aluCalRepositorio;
+
+	@javax.inject.Inject
+	CalificacionRepositorio califRepositorio;
+
+	@javax.inject.Inject
+	PeriodoRepositorio perRepo;
 }
