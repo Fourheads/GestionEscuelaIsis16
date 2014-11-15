9005cb38bfc162fdcb3c5ab548d1860465248ff7
Mingorth <Mingorth@gmail.com>
2014-11-15 15:44:01 -0300

    Clase HojalibroDiarioservice: Se acomodo la clase para mejro uso.

--- dom/src/main/java/dom/libroDiario/HojaLibroDiarioSevice.java
+++ dom/src/main/java/dom/libroDiario/HojaLibroDiarioSevice.java
@@ -30,18 +30,25 @@ import org.apache.isis.applib.annotation.DomainService;
 import org.apache.isis.applib.annotation.Hidden;
 import org.apache.isis.applib.annotation.MemberOrder;
 import org.apache.isis.applib.annotation.Named;
+import org.apache.isis.applib.annotation.NotContributed;
 import org.apache.isis.applib.annotation.PublishedAction;
 import org.apache.isis.applib.services.memento.MementoService;
 import org.apache.isis.applib.services.memento.MementoService.Memento;
 import org.joda.time.LocalDate;
 
 
+
+
+
+import dom.planEstudio.Plan;
 import dom.simple.Curso;
+import dom.simple.CursoRepositorio;
 
 @Hidden
 @DomainService()
 public class HojaLibroDiarioSevice {
 
+	
 	@Hidden
 	public String getId() {
 		return "hojalibrodiario";
@@ -54,49 +61,43 @@ public class HojaLibroDiarioSevice {
 	public String iconName() {//Ver que onda??
 		return "SimpleObject";
 	}
-		
+	
+
+	private ValoresHojadellibro values=new ValoresHojadellibro();
 	
 	//@MemberOrder(name = "Hoja del libro",sequence = "10")
 	@PublishedAction
-	public void Crearview(LibroDiario newlibro,LocalDate fecha) {
-
-		Memento memento = mementoService.create();
+	public List<Hojadellibro> Crearview(String plan,int anio, String division,LocalDate fecha) 
+	{
+		LocalDate temporallocal=new LocalDate();
+		values.setMin(fecha);
+		values.setMax(temporallocal);
 		
-		memento.set("Titulo", fecha);
-		memento.set("Librodiario", newlibro);
-		
-		HojaLibroDiarioView newhoja=new HojaLibroDiarioView();
-		
-		newhoja.viewModelInit(memento.asString());
-		newhoja.setListahojadellibro(this.returnListhojadellibro(newlibro));
-		
-		//return container.newViewModelInstance(
-			//	HojaLibroDiarioView.class, memento.asString());
-		 container.persist(newhoja);
+		return returnListhojadellibro(traerLibro(traercurso(crealistacurso(anio, division, creaplan(plan)), anio, division)),fecha);
 	}
 
-
-	public List<Hojadellibro> returnListhojadellibro(LibroDiario newlibro)
+	@Hidden
+	public List<Hojadellibro> returnListhojadellibro(LibroDiario newlibro, LocalDate fecha)
 	{
 		List<Hojadellibro> Listhojadellibro=new ArrayList<Hojadellibro>(); 
 		
-		for(MateriaDelLibroDiario matelibro:newlibro.getMateriaDelLibroDiarioList())
+		for(MateriaDelLibroDiario matelibro:crealistamateria(newlibro))
 		{
 			
-			for(EntradaLibroDiario entralibro:matelibro.getEntradaLibroDiario())
+			for(EntradaLibroDiario entralibro:crealistaentrada(matelibro))
 			{
-				for(HoraCatedra horacete:entralibro.getHoraCatedra())
+				if(entralibro.getFecha().isBefore(values.getMax()))
+					values.setMin(entralibro.getFecha());
+				
+				if(entralibro.getFecha().isAfter(values.getMin()))
+					values.setMax(entralibro.getFecha());
+			
+				if(entralibro.getFecha().toString().contains(fecha.toString()))
 				{
-					Hojadellibro newhojalibro=new Hojadellibro();
-					
-					newhojalibro.setMateria(matelibro);
-					newhojalibro.setFecha(entralibro.getFecha());
-					newhojalibro.setActividades(horacete.getActividad());
-					newhojalibro.setObservaciones(horacete.getObservaciones());
-					newhojalibro.setUnidad(horacete.getUnidad());
-					newhojalibro.setHora(horacete.getNumerodehora());
-					
-					Listhojadellibro.add(newhojalibro);
+					for(HoraCatedra horacete:crealistahora(entralibro))
+					{	
+						Listhojadellibro.add(creanewHoja(matelibro, horacete));
+					}
 				}
 			}
 		}
@@ -104,10 +105,144 @@ public class HojaLibroDiarioSevice {
 		return Listhojadellibro;
 	}
 	
+	@Hidden
+	public void valoresHojalibro(final LibroDiario newlibro,final LocalDate fecha)
+	{
+		if(newlibro!=null)
+		{
+			List<Integer> horas=new ArrayList<Integer>();
+			for(MateriaDelLibroDiario matelibro:crealistamateria(newlibro))
+			{
+				for(EntradaLibroDiario entralibro:crealistaentrada(matelibro))
+					{
+		
+						if(entralibro.getFecha().equals(fecha))
+						{
+							for(HoraCatedra horacete:crealistahora(entralibro))
+							{	
+								horas.add(horacete.getNumerodehora());
+								values.addUnidaddisponibles(horacete.getUnidad());
+								values.addHorasdisponibles(horacete.getNumerodehora());
+							}
+						}
+					}
+			}
+			//values.getUnidaddisponibles().remove(0);
+			values.setHorasdisponibles(horas);
+		}
+	}
+	
+	@Hidden
+	public List<Integer> unidades()
+	{
+		List<Integer> enteros=new ArrayList<Integer>();
+		
+		if(values.getUnidaddisponibles()!=null)
+			enteros=values.getUnidaddisponibles();
+		return enteros;
+	}
+	
+	@Hidden
+	public List<Integer> horas()
+	{
+		List<Integer> enteros=new ArrayList<Integer>();
+		
+		if(values.getHorasdisponibles()!=null)
+			enteros=values.getHorasdisponibles();
+		return enteros;
+	}
+	
+	@Hidden
+	public LocalDate maxvalue()
+	{
+	 return values.getMax();
+	}
+	
+	@Hidden
+	public LocalDate minvalue()
+	{
+		return values.getMin();
+	}
+	
+	private Plan creaplan(String plan)
+	{
+		Plan newplan=new Plan();
+		newplan.setDescripcion(plan);
+		
+		return newplan;
+	}
+	
+	private List<Curso> crealistacurso(int anio, String divicion, Plan plan)
+	{
+		List<Curso> listacurso=new ArrayList<Curso>();
+		listacurso=cursorepo.listarCursosDeUnPlan(plan);
+		return listacurso;
+	}
+	
+	private Curso traercurso(List<Curso> listacurso, int anio, String division)
+	{
+		Curso newCurso=new Curso();
+		
+		for(Curso curso:listacurso){
+			
+			if(curso.getAnio().getAnioNumero()==anio && curso.getDivision().contains(division))
+			{
+				newCurso=curso;
+			}
+		}
+		return newCurso;
+	}
+	
+	private LibroDiario traerLibro(Curso curso)
+	{
+		LibroDiario newlibro=new LibroDiario();
+		newlibro=librorepo.mostrarLibroDiarioDelCurso(curso);
+		return newlibro;
+	}
+
+	
+	private List<MateriaDelLibroDiario> crealistamateria(LibroDiario libro)
+	{
+		List<MateriaDelLibroDiario> listamateria=new ArrayList<MateriaDelLibroDiario>();
+		listamateria=libro.getMateriaDelLibroDiarioList();
+		return listamateria;
+	}
+		
+	private List<EntradaLibroDiario> crealistaentrada(MateriaDelLibroDiario materia)
+	{
+		 List<EntradaLibroDiario> listaentradalibro=new ArrayList<EntradaLibroDiario>();
+		 listaentradalibro=materia.getEntradaLibroDiario();
+		 return listaentradalibro;
+	}
+	
+	private List<HoraCatedra> crealistahora(EntradaLibroDiario entradalibro)
+	{
+		List<HoraCatedra> listahoracatedra=new ArrayList<HoraCatedra>();
+		listahoracatedra=entradalibro.getHoraCatedra();
+		return listahoracatedra;
+	}
+	
+	private Hojadellibro creanewHoja(MateriaDelLibroDiario matelibro, HoraCatedra horacete)
+	{
+		Hojadellibro newhojalibro=new Hojadellibro();
+		
+		newhojalibro.setMateria(matelibro);
+		newhojalibro.setActividades(horacete.getActividad());
+		newhojalibro.setObservaciones(horacete.getObservaciones());
+		newhojalibro.setUnidad(horacete.getUnidad());
+		newhojalibro.setHora(horacete.getNumerodehora());
+		
+		return newhojalibro;
+	}
+	
+	
+	
 	@javax.inject.Inject
 	DomainObjectContainer container;
 	@javax.inject.Inject
 	MementoService mementoService;
 	@javax.inject.Inject
 	LibroDiarioRepositorio librorepo;
+	@javax.inject.Inject
+	CursoRepositorio cursorepo;
 }
