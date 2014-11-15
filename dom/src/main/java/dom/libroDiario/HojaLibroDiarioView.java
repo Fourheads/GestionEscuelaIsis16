8ad156d484df438465b2c8b93734ee36b3d7bac7
Mingorth <Mingorth@gmail.com>
2014-11-15 12:14:50 -0300

    Clase HojaLibroDiarioView: Funcionando.
    -se cambio la propiedad title para dentificar mejor el view.
    -se agrego la propiedad hojadellibro que es una lista de hojas.
    -se agrearon propiedades de fecha, una para la fecha elegida y otras 2
    siendo estas las fechas maximas y minimas en las que se podra navegar.
    -se agregaron las propiedades, anio, division, plan para poder crear el view
    -se agregaron metodos programados(@programatic) para los botones dia anterior
    y siguente dia. Tambien acompañados de estos esta el metodo NuevoViewModel
    que es el responsable de crear una nueva instancia de este con una fecha nueva.

--- dom/src/main/java/dom/libroDiario/HojaLibroDiarioView.java
+++ dom/src/main/java/dom/libroDiario/HojaLibroDiarioView.java
@@ -28,19 +28,26 @@ import java.util.List;
 import javax.jdo.annotations.Column;
 
 import org.apache.isis.applib.AbstractViewModel;
+import org.apache.isis.applib.DomainObjectContainer;
 import org.apache.isis.applib.annotation.Bookmarkable;
 import org.apache.isis.applib.annotation.Hidden;
 import org.apache.isis.applib.annotation.MemberGroupLayout;
 import org.apache.isis.applib.annotation.MemberOrder;
 import org.apache.isis.applib.annotation.Named;
+import org.apache.isis.applib.annotation.Programmatic;
+import org.apache.isis.applib.annotation.PublishedAction;
+import org.apache.isis.applib.annotation.Render;
+import org.apache.isis.applib.annotation.Render.Type;
 import org.apache.isis.applib.services.memento.MementoService;
 import org.apache.isis.applib.services.memento.MementoService.Memento;
 import org.joda.time.LocalDate;
 
+import dom.asistencia.TomarAsistenciaView;
+import dom.simple.Curso;
+
 @Named("Hoja del libro diario")
 @Bookmarkable
 @MemberGroupLayout(columnSpans = { 6, 0, 6, 12 })
-
 public class HojaLibroDiarioView extends AbstractViewModel {
 
 	@Hidden
@@ -48,7 +55,7 @@ public class HojaLibroDiarioView extends AbstractViewModel {
 		return viewModelMemento();
 	}
 
-	private String title="Fecha: ";
+	private String title="Hoja del libro dairio para el curso: ";
 
 	// region > identification in the UI
 	public String title() {
@@ -66,6 +73,105 @@ public class HojaLibroDiarioView extends AbstractViewModel {
 		return memento;
 	}
 
+	// {{ Listahojadellibro (property)
+	private List<Hojadellibro> hojadellibro;
+
+	@Render(Type.EAGERLY)
+	@MemberOrder(sequence = "2")
+	@Named("Registro de clases dictadas")
+	public List<Hojadellibro> getListahojadellibro() {
+		return hojadellibro;
+	}
+
+	public void setListahojadellibro(final List<Hojadellibro> hojadellibro) {
+		this.hojadellibro = hojadellibro;
+	}
+
+	// }}
+
+	// {{ Fecha (property)
+	private LocalDate fecha;
+
+
+	@MemberOrder(sequence = "1")
+	public LocalDate getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(final LocalDate fecha) {
+		this.fecha = fecha;
+	}
+	// }}
+
+	// {{ Min (property)
+	private LocalDate min;
+
+	@Hidden
+	public LocalDate getMin() {
+		return min;
+	}
+
+	public void setMin(final LocalDate min) {
+		this.min = min;
+	}
+	// }}
+
+	// {{ Max (property)
+	private LocalDate max;
+
+	@Hidden
+	public LocalDate getMax() {
+		return max;
+	}
+
+	public void setMax(final LocalDate max) {
+		this.max = max;
+	}
+	// }}
+
+
+	// {{ Año (property)
+	private int anio;
+
+	@Hidden
+	@MemberOrder(sequence = "1")
+	public int getAño() {
+		return anio;
+	}
+
+	public void setAño(final int anio) {
+		this.anio = anio;
+	}
+	// }}
+
+	// {{ Division (property)
+	private String divicion;
+
+	@Hidden
+	@MemberOrder(sequence = "1")
+	public String getDivision() {
+		return divicion;
+	}
+
+	public void setDivision(final String divicion) {
+		this.divicion = divicion;
+	}
+	// }}
+
+	// {{ Plan (property)
+	private String plan;
+
+	@Hidden
+	@MemberOrder(sequence = "1")
+	public String getPlan() {
+		return plan;
+	}
+
+	public void setPlan(final String plan) {
+		this.plan = plan;
+	}
+	// }}
+	
 	@Override
 	public void viewModelInit(String mementoString) {
 		this.memento = mementoString;
@@ -73,45 +179,84 @@ public class HojaLibroDiarioView extends AbstractViewModel {
 
 		Memento memento = mementoService.parse(mementoString);
 		
-		title = title+memento.get("Titulo", String.class);
-		setLibrodiario(memento.get("Librodiario", LibroDiario.class));
+		setAño(memento.get("Anio", Integer.class));
+		setDivision(memento.get("Division", String.class));
+		setFecha(memento.get("Fecha", LocalDate.class));
+		setPlan(memento.get("Plan", String.class));
 		
-		setListahojadellibro(this.hojalibrodiarioservice.returnListhojadellibro(this.getLibrodiario()));
-		
+		title = title+getAño()+"º "+getDivision()+" Plan:"+getPlan();
+	
+		setListahojadellibro(this.hojalibrodiarioservice.Crearview(getPlan(), getAño(), getDivision(), getFecha()));
+		setMax(this.hojalibrodiarioservice.maxvalue());
+		setMin(this.hojalibrodiarioservice.minvalue());
 	}
 
-// {{ Listahojadellibro (property)
-private List<Hojadellibro> hojadellibro;
-
-@MemberOrder(sequence = "1")
-public List<Hojadellibro> getListahojadellibro() {
-	return hojadellibro;
-}
-
-public void setListahojadellibro(final List<Hojadellibro> hojadellibro) {
-	this.hojadellibro = hojadellibro;
-}
-
-// {{ Librodiario (property)
-private LibroDiario librodiarioe;
-
-@MemberOrder(sequence = "1")
-public LibroDiario getLibrodiario() {
-	return librodiarioe;
-}
-
-public void setLibrodiario(final LibroDiario librodiarioe) {
-	this.librodiarioe = librodiarioe;
-}
-// }}
-
-
-
-// }}
-
-
 	
 	
+	@Named("Siguiente dia")
+	@PublishedAction
+	public HojaLibroDiarioView pasarAldiasiguiente() {
+
+		return NuevoViewModel(DiaSiguente());
+	}
+
+
+	@Named("Dia anterior")
+	@PublishedAction
+	public HojaLibroDiarioView pasarAldiaanterior() {
+						
+		return NuevoViewModel(DiaAnterior());
+	}
+	
+	
+	@Programmatic
+	private HojaLibroDiarioView NuevoViewModel(LocalDate newIndex) {
+		
+		
+		
+		Memento memento = mementoService.create();
+
+		memento.set("Anio", getAño());
+		memento.set("Division", getDivision());
+		memento.set("Fecha", newIndex);
+		memento.set("Plan", getPlan());
+
+		return container.newViewModelInstance(HojaLibroDiarioView.class,
+				memento.asString());
+	}
+	
+	
+	@Programmatic
+	public LocalDate DiaSiguente() {
+		
+		LocalDate newIndex = getFecha();
+		
+		newIndex=newIndex.plusDays(+1);
+
+		if (newIndex.isAfter(getMax())) {
+			newIndex=min;
+		}
+		
+		return newIndex;
+	}
+	
+	@Programmatic
+	public LocalDate DiaAnterior() {
+		LocalDate newIndex = getFecha();
+		
+		newIndex=newIndex.plusDays(-1);
+		
+		newIndex.plusDays(-1);
+
+		if (newIndex.isBefore(getMin())) {
+			newIndex=max;
+		}
+		
+		return newIndex;
+	}
+	
+	@javax.inject.Inject
+	DomainObjectContainer container;
 	@javax.inject.Inject
 	MementoService mementoService;
 	@javax.inject.Inject
